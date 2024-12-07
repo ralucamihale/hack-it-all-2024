0c99cbb8fe20c886e753b69773258d9742d29835
RomeoBaros <dangerouslobster@yahoo.com>
2024-12-07 13:32:48 +0200

    Start template

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import webbrowser
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/register_mentor')
+def register_mentor():
+    return render_template('register_mentor.html')
+
+@app.route('/register_user')
+def register_user():
+    return render_template('register_user.html')
+
+
+
+def open_browser():
+    webbrowser.open("http://127.0.0.1:8080/")
+
+if __name__ == '__main__':
+    open_browser()
+    app.run(port=8080)
